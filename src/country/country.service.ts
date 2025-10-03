cd2b8ae0ff840be8aba95615a323e57cc8ddf146
Willzera <113561581+Wssfilho@users.noreply.github.com>
2025-10-03 17:00:45 -0300

    service: create service "create" - OK

--- src/country/country.service.ts
+++ src/country/country.service.ts
@@ -1,12 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Response } from '@nestjs/common';
 import { PrismaService } from 'src/databases/prisma.service';
+import { CountryDto } from './dto/country.dto';
 
 @Injectable()
 export class CountryService {
 
-    constructor(private readonly prismaService : PrismaService){}
-    async getAll()
-    {
+    constructor(private readonly prismaService: PrismaService) { }
+    async getAll() {
         const country = await this.prismaService.country.findMany({
             relationLoadStrategy: 'join',
             include: {
@@ -14,6 +14,14 @@ export class CountryService {
             }
 
         })
+        return country;
+    }
+    async create(data: CountryDto) {
+        const country = this.prismaService.country.create(
+            {
+                data,
+            }
+        );
         return country;
     }
 
