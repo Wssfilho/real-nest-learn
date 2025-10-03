e760b97d79b4046657653d30aa3aa925146e1bfd
Willzera <113561581+Wssfilho@users.noreply.github.com>
2025-10-03 17:01:12 -0300

    controller country: create router "create" - OK

--- src/country/country.controller.ts
+++ src/country/country.controller.ts
@@ -1,14 +1,35 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, HttpException, HttpStatus, Post, Res, } from '@nestjs/common';
 import { CountryService } from './country.service';
+import { CountryDto } from './dto/country.dto';
 
 @Controller('country')
 export class CountryController {
 
-    constructor(private readonly coutryService: CountryService){}
+    constructor(private readonly coutryService: CountryService) { }
 
     @Get()
-    async getAll()
-    {
-        return this.coutryService.getAll();
+    async getAll() {
+        const country = await this.coutryService.getAll();
+        if (!country) {
+            throw new HttpException('country not created', HttpStatus.BAD_REQUEST);
+
+        }
+        return {
+            sucess: true,
+            message: 'found',
+            data: country,
+        }
+    }
+    @Post()
+    async create(@Body() data: CountryDto, @Res() res: Response) {
+        const country = this.coutryService.create(data)
+        if (!country) {
+            throw new HttpException('country not created', HttpStatus.NOT_FOUND);
+        }
+        return {
+            success: true,
+            message: 'created',
+            data: country,
+        };
     }
 }
