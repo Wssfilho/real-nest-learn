733222113ac3aa24ecac3155c80989e5a4e1f0ef
Willzera <113561581+Wssfilho@users.noreply.github.com>
2025-10-06 19:54:57 -0300

    feat: adicionar MissionsModule ao AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { HeroModule } from './hero/hero.module';
 import { CountryModule } from './country/country.module';
+import { MissionsModule } from './missions/missions.module';
 
 @Module({
   //se tiver varios modulos podemos colocar tmb 
-  imports: [HeroModule, CountryModule],
+  imports: [HeroModule, CountryModule, MissionsModule],
 })
 export class AppModule {}
