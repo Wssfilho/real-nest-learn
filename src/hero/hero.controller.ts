795b077382a542404f72aafcafe34aa7cfe03cea
Willzera <113561581+Wssfilho@users.noreply.github.com>
2025-10-08 19:00:21 -0300

    refactor: ajustar formatação e remover dto heroPowerDto do HeroController

--- src/hero/hero.controller.ts
+++ src/hero/hero.controller.ts
@@ -1,39 +1,37 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { HeroDto } from '../dtos/hero.dto';
 import { HeroService } from './hero.service';
-import { heroPowerDto } from 'src/dtos/heroPower.dto';
 
 @Controller('hero')
 export class HeroController {
     //so imnporta o servico
-    constructor(private readonly heroService: HeroService){}
+    constructor(private readonly heroService: HeroService) { }
     @Post()
-    async create(@Body() data: HeroDto ){
+    async create(@Body() data: HeroDto) {
         return this.heroService.create(data);
     }
     @Get()
-    async findAll()
-    {
+    async findAll() {
         return this.heroService.findAll();
     }
     //forma de usar o parametro passado pela url
     @Put(":id")
-    async update(@Param("id") id:number, @Body( ) data:HeroDto){
-        return this.heroService.update(Number (id), data);
+    async update(@Param("id") id: number, @Body() data: HeroDto) {
+        return this.heroService.update(Number(id), data);
     }
-    @Delete("deleteHeroPower")
-    async deleteheroPower(@Body() data: heroPowerDto){
-        return this.heroService.removePower(data);
+    @Delete(":heroId/power/:powerId")
+    async deleteheroPower(
+        @Param("heroId") heroId: number, @Param("powerId") powerId: number) {
+        return this.heroService.removePower(Number(heroId), Number(powerId));
     }
     @Delete(":id")
-    async delete(@Param("id") id: number){
+    async delete(@Param("id") id: number) {
         return this.heroService.delete(Number(id));
     }
-    
+
     @Get(":id")
-    async getById(@Param("id") id: number)
-    {
-        return this.heroService.getById(Number (id));
+    async getById(@Param("id") id: number) {
+        return this.heroService.getById(Number(id));
     }
 
 }
