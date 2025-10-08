61e09a8cca2c164165d4f8e4a6743c32e50977c5
Willzera <113561581+Wssfilho@users.noreply.github.com>
2025-10-08 19:20:09 -0300

    feat: adicionar PrismaService como provedor no PowerModule

--- src/power/power.module.ts
+++ src/power/power.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PowerController } from './power.controller';
 import { PowerService } from './power.service';
+import { PrismaService } from 'src/databases/prisma.service';
 
 @Module({
   controllers: [PowerController],
-  providers: [PowerService]
+  providers: [PowerService, PrismaService]
 })
 export class PowerModule {}
