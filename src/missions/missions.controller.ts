acd825f628f91fcfaf71f061bc3707884d7f9970
Willzera <113561581+Wssfilho@users.noreply.github.com>
2025-10-08 18:52:27 -0300

    refactor: remover módulo e serviços de missões

--- src/missions/missions.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { MissionsService } from './missions.service';
-
-@Controller('missions')
-export class MissionsController {
-
-    constructor(private readonly missionsService : MissionsService){}
-
-}
--- src/missions/missions.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/databases/prisma.service';
-
-@Injectable()
-export class MissionsService {
-
-    constructor(private readonly prismaService : PrismaService){}
-}
