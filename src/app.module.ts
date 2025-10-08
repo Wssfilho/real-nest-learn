3bb64628e4a8161f70728583712b8cb02f2e2248
Willzera <113561581+Wssfilho@users.noreply.github.com>
2025-10-08 18:51:39 -0300

    fix: remover MissionsModule das importações no AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { HeroModule } from './hero/hero.module';
 import { CountryModule } from './country/country.module';
-import { MissionsModule } from './missions/missions.module';
 
 @Module({
   //se tiver varios modulos podemos colocar tmb 
-  imports: [HeroModule, CountryModule, MissionsModule],
+  imports: [HeroModule, CountryModule],
 })
 export class AppModule {}
