5ecec2876a80012370c88a090ed1add05144f3a2
Willzera <113561581+Wssfilho@users.noreply.github.com>
2025-10-08 19:20:05 -0300

    feat: adicionar método para obter todos os poderes no PowerController

--- src/power/power.controller.ts
+++ src/power/power.controller.ts
@@ -1,8 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { PowerService } from './power.service';
 
 @Controller('power')
 export class PowerController {
 
     constructor(private readonly powerService : PowerService){}
+
+    @Get()
+    async getAll()
+    {
+        return this.powerService.getAll();
+    }
 }
