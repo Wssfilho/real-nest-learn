1e4cd98b85508be9ddee6fc0c945ddbab335f17b
Willzera <113561581+Wssfilho@users.noreply.github.com>
2025-10-09 21:13:07 -0300

    feat: implementar método create no PowerService e adicionar endpoint correspondente no PowerController

--- src/power/power.controller.ts
+++ src/power/power.controller.ts
@@ -1,11 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { PowerService } from './power.service';
+import { powerDto } from 'src/dtos/power.dto';
 
 @Controller('power')
 export class PowerController {
 
     constructor(private readonly powerService : PowerService){}
 
+
+    @Post()
+    async create(@Body() data: powerDto){
+        return this.powerService.create(data);
+    }
     @Get()
     async getAll()
     {
--- src/power/power.service.ts
+++ src/power/power.service.ts
@@ -1,13 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/databases/prisma.service';
+import { powerDto } from 'src/dtos/power.dto';
 
 @Injectable()
 export class PowerService {
 
-    constructor(private readonly prisma: PrismaService){}
+    constructor(private readonly prisma: PrismaService) { }
 
-
-    async getAll(){
+    async create(data: powerDto) {
+        const findPower = await this.prisma.power.findFirst({ where: { id: data.id } });
+        if (findPower) { throw new ConflictException() }
+        await this.prisma.power.create({ data })
+        return {
+            message: "Power created",
+            statusCode: 200,
+        }
+    }
+    async getAll() {
         return this.prisma.power.findMany();
     }
 
