00f377ed7fb3cf602ff54b3994ba343302327f37
Willzera <113561581+Wssfilho@users.noreply.github.com>
2025-11-16 16:39:45 -0300

    style: remover operador de não nulo (!) das propriedades das classes CountryDto, HeroDto e powerDto; desabilitar a inicialização estrita de propriedades no tsconfig.json

--- src/dtos/hero.dto.ts
+++ src/dtos/hero.dto.ts
@@ -1,9 +1,9 @@
 export class HeroDto {
   id?: number; //eu posso nao passar o di
-  civilName!: string;
-  heroName!: string;
-  age!: number;
+  civilName: string;
+  heroName: string;
+  age: number;
   team?: string;
-  countryId!: number;
-  powerId!: number;
+  countryId: number;
+  powerId: number;
 }
--- src/dtos/power.dto.ts
+++ src/dtos/power.dto.ts
@@ -1,5 +1,5 @@
 export class powerDto {
   id?: number;
-  name!: string;
-  description!: string;
+  name: string;
+  description: string;
 }
