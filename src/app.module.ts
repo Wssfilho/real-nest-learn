88d52f0de8a3cedf4cfbae34908ae9d684f1f853
Willzera <113561581+Wssfilho@users.noreply.github.com>
2025-11-18 21:37:21 -0300

    estilo: adicionar módulo Mission às importações no AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
-import { HeroModule } from './hero/hero.module';
-import { CountryModule } from './country/country.module';
-import { PowerModule } from './power/power.module';
+import { Module } from "@nestjs/common";
+import { HeroModule } from "./hero/hero.module";
+import { CountryModule } from "./country/country.module";
+import { PowerModule } from "./power/power.module";
+import { MissionModule } from "./mission/mission.module";
 
 @Module({
   //se tiver varios modulos podemos colocar tmb
-  imports: [HeroModule, CountryModule, PowerModule],
+  imports: [HeroModule, CountryModule, PowerModule, MissionModule],
 })
 export class AppModule {}
